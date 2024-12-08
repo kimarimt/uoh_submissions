3c38f33dbe86b35561a8577f736c0bd6f9ff5eec
Alec German <kimarimt@protonmail.com>
2024-12-08 00:02:23 -0500

    updated error handler middleware

--- part_4/blog-list/src/utils/middleware.js
+++ part_4/blog-list/src/utils/middleware.js
@@ -21,6 +21,10 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: error.message })
   } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
     return res.status(400).json({ error: 'expected `username` to be unique' })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'token invalid' })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({ error: 'token expoired' })
   }
 
   next(error)
