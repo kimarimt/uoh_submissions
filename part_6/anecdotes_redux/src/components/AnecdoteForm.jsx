333600aba4c77ce6849a06836a3bb05168c39147
Alec German <kimarimt@protonmail.com>
2024-12-20 01:57:04 -0500

    finished 6.6-6.7

---
+++ part_6/anecdotes_redux/src/components/AnecdoteForm.jsx
@@ -0,0 +1,34 @@
+import { useDispatch } from 'react-redux'
+import { addAnecdote } from '../features/ancedotesSlice'
+
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const generateId = () =>
+    Number((Math.random() * 1_000_000).toFixed(0))
+
+  const createAnecdote = event => {
+    event.preventDefault()
+
+    const text = event.target.text.value
+    event.target.text.value = ''
+
+    dispatch(addAnecdote(
+      {
+        id: generateId(),
+        text,
+        votes: 0
+      }
+    ))
+  }
+
+  return (
+    <form onSubmit={createAnecdote}>
+      <input name='text' />
+      <button type="submit">Add</button>
+    </form>
+  )
+}
+
+export default AnecdoteForm
--- part_6/anecdotes_redux/src/components/App.jsx
+++ part_6/anecdotes_redux/src/components/App.jsx
@@ -1,6 +1,7 @@
 import { createSelector } from '@reduxjs/toolkit'
 import { useSelector, useDispatch } from 'react-redux'
-import { castVote, addAnecdote } from '../features/ancedotesSlice'
+import { castVote } from '../features/ancedotesSlice'
+import AnecdoteForm from './AnecdoteForm'
 
 const App = () => {
   const sortedAnecdotesByVotes = createSelector(
@@ -11,35 +12,14 @@ const App = () => {
   const anecdotes = useSelector(sortedAnecdotesByVotes)
   const dispatch = useDispatch()
 
-  const generateId = () =>
-    Number((Math.random() * 1_000_000).toFixed(0))
-
   const updateVote = id => {
     dispatch(castVote(id))
-  }
-
-  const createAnecdote = event => {
-    event.preventDefault()
-
-    const text = event.target.text.value
-    event.target.text.value = ''
-
-    dispatch(addAnecdote(
-      {
-        id: generateId(),
-        text,
-        votes: 0
-      }
-    ))
   }
 
   return (
     <div>
       <h1>Ancedotes</h1>
-      <form onSubmit={createAnecdote}>
-        <input name='text' />
-        <button type="submit">Add</button>
-      </form>
+      <AnecdoteForm />
       <ul>
         {anecdotes.map(anecdote => (
           <li key={anecdote.id}>
