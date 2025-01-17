ae1b9dcb6d2b7f55ed1cb041d3710acad4154bb3
Alec German <kimarimt@protonmail.com>
2025-01-17 11:35:49 -0500

    finished 8.7

--- part_8/library_gql/src/index.js
+++ part_8/library_gql/src/index.js
@@ -6,26 +6,26 @@ import { GraphQLError } from 'graphql'
 let authors = [
   {
     name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
     born: 1952,
   },
   {
     name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
+    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
+    born: 1963,
   },
   {
     name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
+    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
+    born: 1821,
   },
-  { 
-    name: 'Joshua Kerievsky', 
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  {
+    name: 'Joshua Kerievsky',
+    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
   },
-  { 
+  {
     name: 'Sandi Metz',
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
   },
 ]
 
@@ -34,50 +34,50 @@ let books = [
     title: 'Clean Code',
     published: 2008,
     author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
+    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
+    genres: ['refactoring'],
   },
   {
     title: 'Agile software development',
     published: 2002,
     author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
+    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
+    genres: ['agile', 'patterns', 'design'],
   },
   {
     title: 'Refactoring, edition 2',
     published: 2018,
     author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
+    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
+    genres: ['refactoring'],
   },
   {
     title: 'Refactoring to patterns',
     published: 2008,
     author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
+    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
+    genres: ['refactoring', 'patterns'],
+  },
   {
     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
     published: 2012,
     author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
+    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
+    genres: ['refactoring', 'design'],
   },
   {
     title: 'Crime and punishment',
     published: 1866,
     author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
+    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
+    genres: ['classic', 'crime'],
   },
   {
     title: 'Demons',
     published: 1872,
     author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
+    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
+    genres: ['classic', 'revolution'],
   },
 ]
 
@@ -115,6 +115,10 @@ const typeDefs = `
       name: String!
       born: Int
     ): Author
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -123,8 +127,8 @@ const addAuthor = (root, args) => {
     throw new GraphQLError('author already exists', {
       extensions: {
         code: 'BAD_USER_INPUT',
-        invalidArgs: args.name
-      }
+        invalidArgs: args.name,
+      },
     })
   }
 
@@ -141,7 +145,9 @@ const resolvers = {
       const { author, genre } = args
 
       if (author && genre) {
-        return books.filter(b => b.author === author && b.genres.includes(genre))
+        return books.filter(
+          b => b.author === author && b.genres.includes(genre)
+        )
       } else if (author) {
         return books.filter(b => b.author === author)
       } else if (genre) {
@@ -161,8 +167,8 @@ const resolvers = {
         throw new GraphQLError('book already exists', {
           extensions: {
             code: 'BAD_USER_INPUT',
-            invalidArgs: args.title
-          }
+            invalidArgs: args.title,
+          },
         })
       }
 
@@ -175,7 +181,18 @@ const resolvers = {
       return book
     },
     addAuthor,
-  }
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => (a.name === args.name ? updatedAuthor : a))
+      return updatedAuthor
+    },
+  },
 }
 
 const server = new ApolloServer({
