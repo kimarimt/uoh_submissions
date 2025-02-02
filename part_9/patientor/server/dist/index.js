b5450cbc6aa564ff654f8c18d925b3223cb90d67
Alec German <kimarimt@protonmail.com>
2025-02-02 13:51:56 -0500

    finished 9.9

--- part_9/patientor/server/dist/index.js
+++ part_9/patientor/server/dist/index.js
@@ -4,8 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const PORT = 3000;
+const cors_1 = __importDefault(require("cors"));
+const PORT = 3001;
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.get('/api/ping', (_req, res) => {
     res.send('pong');
 });
--- part_9/patientor/server/src/index.ts
+++ part_9/patientor/server/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
